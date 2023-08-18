a1f66f3ea2eb96ade80e2b8097a03cbf4c50279f
Ilingu <ilingu@duck.com>
2023-08-18 11:34:44 +0200

    [2016] day-1 part 2 Discovered the pain to create a dynamic array in c

--- 2016/day-1/main.c
+++ 2016/day-1/main.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
@@ -15,11 +16,11 @@ int clamp(int x, int y, int z)
   return MIN(MAX(x, y), z);
 };
 
-struct Pos
+typedef struct
 {
   int x;
   int y;
-};
+} Pos;
 
 char *getFile(const char *__restrict__ __filename)
 {
@@ -59,6 +60,46 @@ char *getFile(const char *__restrict__ __filename)
   return file_contents;
 }
 
+void updatePos(Pos *curr_pos, int curr_orientation)
+{
+  switch (curr_orientation)
+  {
+  case 0:
+  case 360:
+    (*curr_pos).x += 1;
+    break;
+
+  case 90:
+    (*curr_pos).y += 1;
+    break;
+
+  case 180:
+    (*curr_pos).x -= 1;
+    break;
+
+  case 270:
+    (*curr_pos).y -= 1;
+    break;
+
+  default:
+    exit(1);
+    break;
+  }
+}
+
+bool contains(Pos *seenPos, Pos search, unsigned int length)
+{
+  for (unsigned int i = 0; i < length; i++)
+  {
+    Pos curr = seenPos[i];
+    if (curr.x == search.x && curr.y == search.y)
+    {
+      return true;
+    }
+  }
+  return false;
+}
+
 int main()
 {
   char *file_contents = getFile("input.txt");
@@ -68,53 +109,75 @@ int main()
   }
 
   // init position
-  struct Pos curr_pos;
+  Pos curr_pos;
   curr_pos.x = 0;
   curr_pos.y = 0;
   int curr_orientation = 90;
+
+  // init already seen position dynamic array
+  unsigned int length = 0;
+  Pos *seenPos = (Pos *)malloc(1 * sizeof(Pos));
+
+  if (seenPos == NULL)
+  {
+    printf("Memory not allocated.\n");
+    return EXIT_FAILURE;
+  }
+
+  // set the first seen position
+  seenPos[0] = curr_pos;
+  length++;
+
+  int p2 = -1;
 
   // Split the instructions and interate over it
   char *token = strtok(file_contents, ", ");
   while (token != NULL)
   {
+    // parse the number of block to move
     int numberOfBlocks = atoi(&token[1]);
+    // update the orientation
     if (token[0] == 'R')
       curr_orientation = (curr_orientation + 270) % 360;
     else
       curr_orientation = (curr_orientation + 450) % 360;
 
-    switch (curr_orientation)
+    // update the position according to the current orientation and number of blocks to move
+    for (int i = 0; i < numberOfBlocks; i++)
     {
-    case 0:
-    case 360:
-      curr_pos.x += numberOfBlocks;
-      break;
+      updatePos(&curr_pos, curr_orientation);
 
-    case 90:
-      curr_pos.y += numberOfBlocks;
-      break;
+      // check if new pos is already seen
+      if (p2 == -1 && contains(seenPos, curr_pos, length))
+      {
+        p2 = abs(0 - curr_pos.x) + abs(0 - curr_pos.y);
+      }
 
-    case 180:
-      curr_pos.x -= numberOfBlocks;
-      break;
+      // push new seen pos
+      seenPos = (Pos *)realloc(seenPos, (length + 1) * sizeof(Pos)); // add one to the capacity of the array
+      if (!seenPos)
+      {
+        printf("Memory Re-allocation failed\n");
+        free(file_contents);
+        free(seenPos);
+        return EXIT_FAILURE;
+      }
 
-    case 270:
-      curr_pos.y -= numberOfBlocks;
-      break;
-
-    default:
-      return EXIT_FAILURE;
-      break;
+      seenPos[length] = curr_pos; // push new pos
+      length++;                   // update array length
     }
 
-    token = strtok(NULL, ", "); // Retrieve next token
+    // Retrieve next token
+    token = strtok(NULL, ", ");
   }
 
   // Clean up
   free(file_contents);
+  free(seenPos);
 
   int shortest_dist = abs(0 - curr_pos.x) + abs(0 - curr_pos.y);
-  printf("%d\n", shortest_dist);
+  printf("p1: %d\n", shortest_dist);
+  printf("p2: %d\n", p2);
 
   return EXIT_SUCCESS;
 }
