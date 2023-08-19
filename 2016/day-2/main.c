b2a059c2d676245061951b37b42203bddf343d6e
Ilingu <ilingu@duck.com>
2023-08-19 16:09:04 +0200

    [2016] day2 part2

--- 2016/day-2/main.c
+++ 2016/day-2/main.c
@@ -2,6 +2,16 @@
 #include <string.h>
 #include <stdio.h>
 
+/* HELPERS */
+char *copyString(char s[])
+{
+  char *s2;
+  s2 = (char *)malloc(strlen(s));
+
+  strcpy(s2, s);
+  return (char *)s2;
+}
+
 char *getFile(const char *__restrict__ __filename)
 {
   FILE *file = fopen(__filename, "r");
@@ -42,6 +52,7 @@ char *getFile(const char *__restrict__ __filename)
 
 const char separator[2] = "\n";
 
+/* PART1 */
 char *part1(char *file_contents)
 {
   // init current position
@@ -104,6 +115,74 @@ char *part1(char *file_contents)
   return p1;
 }
 
+/* PART2 */
+const char diamond[5][5] =
+    {{'\0', '\0', '1', '\0', '\0'},
+     {'\0', '2', '3', '4', '\0'},
+     {'5', '6', '7', '8', '9'},
+     {'\0', 'A', 'B', 'C', '\0'},
+     {'\0', '\0', 'D', '\0', '\0'}};
+
+typedef struct Position
+{
+  unsigned short x;
+  unsigned short y;
+} Pos;
+
+char *part2(char *file_contents)
+{
+  // init current position
+  Pos currPos;
+  currPos.x = 0;
+  currPos.y = 2;
+
+  // results
+  char *p2 = (char *)malloc(0);
+  if (p2 == NULL)
+    return NULL;
+
+  // iterate over intructions and update position
+  unsigned int line = 0;
+  char *token = strtok(file_contents, separator);
+  while (token != NULL)
+  {
+    for (int i = 0; token[i] != '\0'; i++)
+    {
+      char direction = token[i];
+      Pos tempPos = currPos;
+      if (direction == 'U')
+        tempPos.y -= 1;
+      else if (direction == 'D')
+        tempPos.y += 1;
+      else if (direction == 'L')
+        tempPos.x -= 1;
+      else if (direction == 'R')
+        tempPos.x += 1;
+      else
+        return NULL;
+
+      if (tempPos.x >= 0 && tempPos.x <= 4 && tempPos.y >= 0 && tempPos.y <= 4 && diamond[tempPos.y][tempPos.x] != '\0')
+        currPos = tempPos;
+    }
+
+    p2 = (char *)realloc(p2, (line + 1) * sizeof(char)); // add one to the capacity of the array
+    if (!p2)
+    {
+      printf("Memory Re-allocation failed\n");
+      free(p2);
+      return NULL;
+    }
+    p2[line] = diamond[currPos.y][currPos.x];
+
+    // Retrieve next token
+    token = strtok(NULL, separator);
+    line++;
+  }
+
+  return p2;
+}
+
+// result
 int main()
 {
   char *file_contents = getFile("input.txt");
@@ -112,17 +191,25 @@ int main()
     return EXIT_FAILURE;
   }
 
-  char *p1 = part1(file_contents);
+  char *p1 = part1(copyString(file_contents));
   if (p1 == NULL)
   {
     return EXIT_FAILURE;
   }
 
-  printf("%s\n", p1);
+  char *p2 = part2(copyString(file_contents));
+  if (p2 == NULL)
+  {
+    return EXIT_FAILURE;
+  }
+
+  printf("part1: %s\n", p1);
+  printf("part2: %s\n", p2);
 
   // clean up
   free(file_contents);
   free(p1);
+  free(p2);
 
   return EXIT_SUCCESS;
 }
